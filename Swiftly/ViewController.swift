bb050de506f4da2198177212f8885635341ae73d
Fruit Lee <glfruit80@gmail.com>
2014-06-04 18:01:23 +0800

    Initial Commit

---
+++ Swiftly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swiftly
+//
+//  Created by Fruit Lee on 14/6/4.
+//  Copyright (c) 2014年 Hua Yan Co. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
